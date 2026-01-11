eebe1a63133f0b7dfe77d93defa84d4008b9c64f
Eric <emekaeric12@gmail.com>
2026-01-11 16:53:22 +0100

    Homepage made to redirect staffs to their dashboard based on their role

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,33 @@
+"use client";
 
-export default function page() {
-  return (
-    <div className=' h-screen'>
+import { useGetRole } from "@/shared/hooks/useGetRole";
+import Loader from "@/shared/ui/Loader";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
-    </div>
-  )
+export default function Page() {
+  const role = useGetRole(); // "admin" | "cashier" | "waiter" | null
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!role) return; // wait until role is loaded
+
+    switch (role) {
+      case "admin":
+        router.push("/admin");
+        break;
+      case "cashier":
+        router.push("/cashier/orders");
+        break;
+      case "waiter":
+        router.push("/waiter/orders");
+        break;
+      default:
+        // Optional: fallback route for unknown role
+        router.push("/login");
+        break;
+    }
+  }, [role, router]);
+
+  return <Loader />;
 }
