9842bb12bfb43ecc0c634e3fc2b4d87e574f679d
Eric <emekaeric12@gmail.com>
2026-01-27 13:23:47 +0100

    Create Menu-Items changes to Create Menu-item

--- screens/dashboard/ui/Header.tsx
+++ screens/dashboard/ui/Header.tsx
@@ -31,7 +31,7 @@ export default function Header() {
           className="flex items-center gap-3 h-12 px-4 w-full lg:w-fit cursor-pointer bg-white border border-orange-500 text-gray-700 rounded-xl shadow-sm hover:bg-orange-50 transition-all duration-300 active:scale-90"
         >
           <UtensilsCrossed size={20} className="text-orange-500 transition-colors duration-300" />
-          <span className="text-sm font-bold tracking-wide">Create Menu-Items</span>
+          <span className="text-sm font-bold tracking-wide">Create Menu-Item</span>
         </button>
       </div>
     </div>
