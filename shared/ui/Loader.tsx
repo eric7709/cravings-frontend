9b8aa4c5f6935fcc9d2f02ad5f7d5938fb135a6b
Eric <emekaeric12@gmail.com>
2026-01-27 11:58:57 +0100

    Bg Removed from loader

--- shared/ui/Loader.tsx
+++ shared/ui/Loader.tsx
@@ -1,6 +1,6 @@
 export default function Loader() {
   return (
-    <div className="flex items-center z-500 bg-white fixed top-0 left-0 inset-0 justify-center h-dvh">
+    <div className="flex items-center z-500  fixed top-0 left-0 inset-0 justify-center h-dvh">
       <div className="w-12 h-12 mt-5 border-4 border-slate-300 border-t-blue-900 rounded-full animate-spin"></div>
     </div>
   );
